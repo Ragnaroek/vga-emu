c59df309b4d91cd480dddf51d787b87e46d81cc7
Michael Bohn <spaceodyssey@gmx.de>
2025-10-01 18:13:05 +0100

    spawn_async util

--- examples/ball/src/main.rs
+++ examples/ball/src/main.rs
@@ -1,25 +1,9 @@
 use vgaball::start_ball;
 
-fn main() -> Result<(), String> {
-    let rt = tokio::runtime::Builder::new_current_thread()
-        .enable_time()
-        .build()
-        .map_err(|e| e.to_string())?;
-
-    rt.block_on(async move {
-        let local = tokio::task::LocalSet::new();
-        local
-            .run_until(async move {
-                tokio::task::spawn_local(async move {
-                    start_ball()
-                        .await
-                        .expect("ball demo finished without error");
-                })
-                .await
-                .unwrap();
-            })
-            .await;
+fn main() {
+    vga::util::spawn_async(async move {
+        start_ball()
+            .await
+            .expect("ball demo finished without error");
     });
-
-    Ok(())
 }
