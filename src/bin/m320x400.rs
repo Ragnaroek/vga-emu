57f272fc5ea3ce8151f9b48de6c0a930455a80cb
Michael Bohn <spaceodyssey@gmx.de>
2025-10-01 18:13:02 +0100

    320x400

--- src/bin/m320x400.rs
+++ src/bin/m320x400.rs
@@ -1,19 +1,41 @@
 //Example from https://www.phatcode.net/res/224/files/html/ch31/31-03.html (LISTING 31.3)
 
 use std::sync::Arc;
+use std::thread;
 
 use vga::screen;
-use vga::{CRTReg, GCReg, GeneralReg, SCReg, AttributeReg};
+use vga::{CRTReg, GCReg, SCReg};
 
-const SCREEN_WIDTH : usize = 80;
+const SCREEN_WIDTH: usize = 320;
+
+struct LineControl {
+	start_x: i16,
+	start_y: i16,
+	x_inc: i16,
+	y_inc: i16,
+	base_len: i16,
+	color: u8,
+}
+
+fn new_line(
+	start_x: i16, start_y: i16, x_inc: i16, y_inc: i16, base_len: i16, color: u8,
+) -> LineControl {
+	LineControl {
+		start_x,
+		start_y,
+		x_inc,
+		y_inc,
+		base_len,
+		color,
+	}
+}
 
 pub fn main() {
-
 	let vga = vga::new(0x13);
 
 	//set 320x400 mode
-	let mem_mode = vga.get_sc_data(SCReg::MemoryMode);	
-	vga.set_sc_data(SCReg::MemoryMode, (mem_mode & !0x08)| 0x04); //turn off chain 4 & odd/even
+	let mem_mode = vga.get_sc_data(SCReg::MemoryMode);
+	vga.set_sc_data(SCReg::MemoryMode, (mem_mode & !0x08) | 0x04); //turn off chain 4 & odd/even
 
 	let gc_mode = vga.get_gc_data(GCReg::GraphicsMode);
 	vga.set_gc_data(GCReg::GraphicsMode, gc_mode & !0x10); //turn off odd/even
@@ -21,8 +43,14 @@ pub fn main() {
 	let gc_misc = vga.get_gc_data(GCReg::MiscGraphics);
 	vga.set_gc_data(GCReg::MiscGraphics, gc_misc & !0x02); //turn off chain
 
+	//clear display memory
+	vga.set_sc_data(SCReg::MapMask, 0x0F);
+	for cx in 0..vga::PLANE_SIZE {
+		vga.write_mem(cx, 0);
+	}
+
 	let max_scan = vga.get_crt_data(CRTReg::MaximumScanLine);
-	vga.set_crt_data(CRTReg::MaximumScanLine, max_scan & !0x1F); 
+	vga.set_crt_data(CRTReg::MaximumScanLine, max_scan & !0x1F);
 
 	let underline = vga.get_crt_data(CRTReg::UnderlineLocation);
 	vga.set_crt_data(CRTReg::UnderlineLocation, underline & !0x40); //turn off doubleword
@@ -30,12 +58,51 @@ pub fn main() {
 	let crt_mode = vga.get_crt_data(CRTReg::CRTCModeControl);
 	vga.set_crt_data(CRTReg::CRTCModeControl, crt_mode & 0x40); //turn on byte mode bit
 
-	vga.set_sc_data(SCReg::MapMask, 0x0F);
-	
-	//clear display memory
-	for cx in 0..vga::PLANE_SIZE {
-		vga.write_mem(cx, 0);
-	}
+	let vga_m = Arc::new(vga);
+	let vga_t = vga_m.clone();
 
-	//TODO How does reg state exactly control rendering? Which settings are importan => chain?
+	let line_list = vec![
+		new_line(130, 110, 1, 0, 60, 0),
+		new_line(190, 110, 1, 1, 60, 1),
+		new_line(250, 170, 0, 1, 60, 2),
+		new_line(250, 230, -1, 1, 60, 3),
+		new_line(190, 290, -1, 0, 60, 4),
+		new_line(130, 290, -1, -1, 60, 5),
+		new_line(70, 230, 0, -1, 60, 6),
+		new_line(70, 170, 1, -1, 60, 7),
+	];
+
+	thread::spawn(move || {
+		for b in 0..8 {
+			for line in &line_list {
+				let mut x = line.start_x;
+				let mut y = line.start_y;
+				for _ in 0..line.base_len {
+					write_pixel(&vga_t, x, y, b + line.color);
+					x += line.x_inc;
+					y += line.y_inc;
+				}
+			}
+
+    		let _ = std::io::stdin().read_line(&mut String::new()).expect("input read failed");
+		}
+	});
+
+	//TODO read key, inc base_color, up to 8 times
+
+	let options: screen::Options = vga::screen::Options {
+		show_frame_rate: true,
+		..Default::default()
+	};
+	screen::start(vga_m, options).unwrap();
+}
+
+fn write_pixel(vga: &vga::VGA, x: i16, y: i16, color: u8) {
+	let mut offset = (SCREEN_WIDTH / 4) * y as usize;
+	offset += x as usize / 4;
+
+	let plane = x & 0x03;
+	let mask = 1 << plane;
+	vga.set_sc_data(SCReg::MapMask, mask);
+	vga.write_mem(offset, color);
 }
--- src/screen.rs
+++ src/screen.rs
@@ -8,14 +8,14 @@ use sdl2::pixels::{Color, PixelFormatEnum};
 use sdl2::rect::Rect;
 use sdl2::ttf;
 
-use super::{CRTReg, GeneralReg, AttributeReg, VGA};
+use super::{AttributeReg, CRTReg, GeneralReg, VGA};
 
 const CLEAR_VR_MASK: u8 = 0b11110111;
 const CLEAR_DE_MASK: u8 = 0b11111110;
 pub const TARGET_FRAME_RATE_MICRO: u128 = 1_000_000 / 60;
-pub const VERTICAL_RESET_MICRO : u64 = 635;
+pub const VERTICAL_RESET_MICRO: u64 = 635;
 
-const DEBUG_HEIGHT: u32 = 20;
+const DEBUG_HEIGHT: usize = 20;
 const FRAME_RATE_SAMPLES: usize = 100;
 
 #[derive(Clone, Copy)]
@@ -37,10 +37,10 @@ impl Default for Options {
 //Shows the screen according to the VGA video mode
 pub fn start(vga: Arc<VGA>, options: Options) -> Result<(), String> {
     let mode = vga.get_video_mode();
-    if mode == 0x10 { //TODO Get rid of this, width and height are derived from register state!!
-        start_video(vga, 640, 350, options)
+    if mode == 0x10 {
+        start_video(vga, 640, 350, options, 1)
     } else if mode == 0x13 {
-        start_video(vga, 320, 200, options)
+        start_video(vga, 640, 400, options, 2)
     } else {
         panic!("video mode {:x}h not implemented", vga.get_video_mode())
     }
@@ -51,16 +51,22 @@ pub fn start(vga: Arc<VGA>, options: Options) -> Result<(), String> {
 //you did not change the default settings)
 pub fn start_debug_planar_mode(
     vga: Arc<VGA>,
-    w: u32,
-    h: u32,
+    w: usize,
+    h: usize,
     options: Options,
 ) -> Result<(), String> {
     let mut debug_options = options;
     debug_options.start_addr_override = Some(0);
-    start_video(vga, w, h, debug_options)
+    start_video(vga, w, h, debug_options, 0)
 }
 
-fn start_video(vga: Arc<VGA>, w: u32, h: u32, options: Options) -> Result<(), String> {
+fn start_video(
+    vga: Arc<VGA>,
+    w: usize,
+    h: usize,
+    options: Options,
+    v_stretch: usize,
+) -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
     let ttf_context = ttf::init().map_err(|e| e.to_string())?;
@@ -88,12 +94,12 @@ fn start_video(vga: Arc<VGA>, w: u32, h: u32, options: Options) -> Result<(), St
     let window = video_subsystem
         .window(
             "VGA",
-            w,
+            w as u32,
             if options.show_frame_rate {
                 h + DEBUG_HEIGHT
             } else {
                 h
-            },
+            } as u32,
         )
         .position_centered()
         .build()
@@ -102,7 +108,7 @@ fn start_video(vga: Arc<VGA>, w: u32, h: u32, options: Options) -> Result<(), St
     let mut event_pump = sdl_context.event_pump()?;
     let texture_creator = canvas.texture_creator();
     let mut texture = texture_creator
-        .create_texture_streaming(PixelFormatEnum::RGB24, w, h)
+        .create_texture_streaming(PixelFormatEnum::RGB24, w as u32, h as u32)
         .map_err(|e| e.to_string())?;
 
     let offset_delta = vga.get_crt_data(CRTReg::Offset) as usize;
@@ -110,50 +116,31 @@ fn start_video(vga: Arc<VGA>, w: u32, h: u32, options: Options) -> Result<(), St
         return Err(format!("illegal CRT offset: {}", offset_delta));
     }
 
+    let vmode = vga.get_video_mode();
+
     'running: loop {
-        let mut mem_offset = mem_offset(&vga, &options);
-        let mut x: usize = 0;
-        let mut y: usize = 0;
+        let mem_offset = mem_offset(&vga, &options);
         let frame_start = Instant::now();
-        
         set_de(&vga, true); //display enable is currently only set for whole frame (not toggled for horizontal retrace)
         texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
-            for _ in 0..(h as usize) {
-                let hpan = vga.get_attribute_reg(AttributeReg::HorizontalPixelPanning) & 0xF; 
-                
-                let w_bytes = ((w / 8) as usize)+1; //+1 for "overshot" with potential hpan
-                for mem_byte in 0..w_bytes { 
-                    let v0 = vga.raw_read_mem(0, mem_offset + mem_byte);
-                    let v1 = vga.raw_read_mem(1, mem_offset + mem_byte);
-                    let v2 = vga.raw_read_mem(2, mem_offset + mem_byte);
-                    let v3 = vga.raw_read_mem(3, mem_offset + mem_byte);
-
-                    let start = if mem_byte == 0 {hpan} else {0};
-                    let end = if mem_byte == w_bytes - 1 {hpan} else {8};
-                    for b in start..end {
-                        let bx = (1 << (7 - b)) as u8;
-                        let mut pixel = bit_x(v0, bx, 0);
-                        pixel |= bit_x(v1, bx, 1);
-                        pixel |= bit_x(v2, bx, 2);
-                        pixel |= bit_x(v3, bx, 3);
-
-                        let color = default_color(pixel);
-                        let offset = y * pitch + x * 3;
-                        buffer[offset] = color.r;
-                        buffer[offset + 1] = color.g;
-                        buffer[offset + 2] = color.b;
-                        x += 1;
-                    }
-                }
-                x = 0;
-                y += 1;
-                mem_offset += offset_delta * 2;
+            if vmode == 0x13 {
+                render_linear(&vga, mem_offset, offset_delta, h, v_stretch, buffer);
+            } else {
+                render_planar(
+                    &vga,
+                    mem_offset,
+                    offset_delta,
+                    w,
+                    h,
+                    buffer,
+                    pitch,
+                );
             }
         })?;
         set_de(&vga, false);
 
         canvas.clear();
-        canvas.copy(&texture, None, Some(Rect::new(0, 0, w, h)))?;
+        canvas.copy(&texture, None, Some(Rect::new(0, 0, w as u32, h as u32)))?;
         if options.show_frame_rate {
             let surface = font
                 .as_ref()
@@ -167,7 +154,7 @@ fn start_video(vga: Arc<VGA>, w: u32, h: u32, options: Options) -> Result<(), St
             canvas.copy(
                 &dbg_texture,
                 None,
-                Some(Rect::new(0, h as i32, 200, DEBUG_HEIGHT)),
+                Some(Rect::new(0, h as i32, 200, DEBUG_HEIGHT as u32)),
             )?;
         }
 
@@ -183,7 +170,6 @@ fn start_video(vga: Arc<VGA>, w: u32, h: u32, options: Options) -> Result<(), St
                 _ => {}
             }
         }
-        
         //simulate vertical reset
         set_vr(&vga, true);
         sleep(Duration::from_micros(VERTICAL_RESET_MICRO));
@@ -206,10 +192,88 @@ fn start_video(vga: Arc<VGA>, w: u32, h: u32, options: Options) -> Result<(), St
         fr_buffer_vsync[fr_ix_vsync] = v_elapsed_vsync;
         fr_sum_vsync += v_elapsed_vsync;
         fr_vsync = 1_000_000 / (fr_sum_vsync / (FRAME_RATE_SAMPLES as u128));
-        
     }
 
     Ok(())
+}
+
+fn render_planar(
+    vga: &VGA,
+    mem_offset_p: usize,
+    offset_delta: usize,
+    w: usize,
+    h: usize,
+    buffer: &mut [u8],
+    pitch: usize,
+) {
+    let mut x: usize = 0;
+    let mut y: usize = 0;
+    let mut mem_offset = mem_offset_p;
+    let max_scan = (vga.get_crt_data(CRTReg::MaximumScanLine) & 0x1F) as usize + 1;
+
+    for _ in 0..(h / max_scan) {
+        for _ in 0..max_scan {
+            let hpan = vga.get_attribute_reg(AttributeReg::HorizontalPixelPanning) & 0xF;
+            let w_bytes = ((w / 8) as usize) + 1; //+1 for "overshot" with potential hpan
+            for mem_byte in 0..w_bytes {
+                let v0 = vga.raw_read_mem(0, mem_offset + mem_byte);
+                let v1 = vga.raw_read_mem(1, mem_offset + mem_byte);
+                let v2 = vga.raw_read_mem(2, mem_offset + mem_byte);
+                let v3 = vga.raw_read_mem(3, mem_offset + mem_byte);
+
+                let start = if mem_byte == 0 { hpan } else { 0 };
+                let end = if mem_byte == w_bytes - 1 { hpan } else { 8 };
+                for b in start..end {
+                    let bx = (1 << (7 - b)) as u8;
+                    let mut pixel = bit_x(v0, bx, 0);
+                    pixel |= bit_x(v1, bx, 1);
+                    pixel |= bit_x(v2, bx, 2);
+                    pixel |= bit_x(v3, bx, 3);
+
+                    let color = default_16_color(pixel);
+                    let offset = y * pitch + x * 3;
+                    buffer[offset] = color.r;
+                    buffer[offset + 1] = color.g;
+                    buffer[offset + 2] = color.b;
+                    x += 1;
+                }
+            }
+            x = 0;
+            y += 1;
+        }
+        mem_offset += offset_delta * 2;
+    }
+}
+
+fn render_linear(
+    vga: &VGA,
+    mem_offset_p: usize,
+    offset_delta: usize,
+    h: usize,
+    v_stretch: usize,
+    buffer: &mut [u8],
+) {
+    let mut mem_offset = mem_offset_p;
+    let max_scan = (vga.get_crt_data(CRTReg::MaximumScanLine) & 0x1F) as usize + 1;
+
+    let mut buffer_offset = 0;
+    for y in 0..((h / max_scan) as usize) {
+        for _ in 0..max_scan {
+            for x_byte in 0..80 {
+                for p in 0..4 {
+                    let v = vga.raw_read_mem(p, mem_offset + x_byte);
+                    let color = DEFAULT_256_COLORS[v as usize];
+                    for _ in 0..v_stretch {
+                        buffer[buffer_offset] = ((color & 0xFF0000) >> 16) as u8;
+                        buffer[buffer_offset + 1] = ((color & 0x00FF00) >> 8) as u8;
+                        buffer[buffer_offset + 2] = (color & 0x0000FF) as u8;
+                        buffer_offset += 3;
+                    }
+                }
+            }
+        }
+        mem_offset += offset_delta * 2;
+    }
 }
 
 fn mem_offset(vga: &VGA, options: &Options) -> usize {
@@ -253,7 +317,7 @@ fn set_de(vga: &VGA, display_mode: bool) {
     }
 }
 
-fn default_color(v: u8) -> Color {
+fn default_16_color(v: u8) -> Color {
     //source: https://wasteland.fandom.com/wiki/EGA_Colour_Palette
     match v {
         0x00 => Color::RGB(0x0, 0x0, 0x0),
@@ -275,3 +339,36 @@ fn default_color(v: u8) -> Color {
         _ => panic!("wrong color index"),
     }
 }
+
+//taken from https://commons.wikimedia.org/wiki/User:Psychonaut/ipalette.sh
+static DEFAULT_256_COLORS: [u32; 256] = [
+    0x000000, 0x0000AA, 0x00AA00, 0x00AAAA, 0xAA0000, 0xAA00AA, 0xAA5500, 0xAAAAAA, 0x555555,
+    0x5555FF, 0x55FF55, 0x55FFFF, 0xFF5555, 0xFF55FF, 0xFFFF55, 0xFFFFFF, 0x000000, 0x101010,
+    0x202020, 0x353535, 0x454545, 0x555555, 0x656565, 0x757575, 0x8A8A8A, 0x9A9A9A, 0xAAAAAA,
+    0xBABABA, 0xCACACA, 0xDFDFDF, 0xEFEFEF, 0xFFFFFF, 0x0000FF, 0x4100FF, 0x8200FF, 0xBE00FF,
+    0xFF00FF, 0xFF00BE, 0xFF0082, 0xFF0041, 0xFF0000, 0xFF4100, 0xFF8200, 0xFFBE00, 0xFFFF00,
+    0xBEFF00, 0x82FF00, 0x41FF00, 0x00FF00, 0x00FF41, 0x00FF82, 0x00FFBE, 0x00FFFF, 0x00BEFF,
+    0x0082FF, 0x0041FF, 0x8282FF, 0x9E82FF, 0xBE82FF, 0xDF82FF, 0xFF82FF, 0xFF82DF, 0xFF82BE,
+    0xFF829E, 0xFF8282, 0xFF9E82, 0xFFBE82, 0xFFDF82, 0xFFFF82, 0xDFFF82, 0xBEFF82, 0x9EFF82,
+    0x82FF82, 0x82FF9E, 0x82FFBE, 0x82FFDF, 0x82FFFF, 0x82DFFF, 0x82BEFF, 0x829EFF, 0xBABAFF,
+    0xCABAFF, 0xDFBAFF, 0xEFBAFF, 0xFFBAFF, 0xFFBAEF, 0xFFBADF, 0xFFBACA, 0xFFBABA, 0xFFCABA,
+    0xFFDFBA, 0xFFEFBA, 0xFFFFBA, 0xEFFFBA, 0xDFFFBA, 0xCAFFBA, 0xBAFFBA, 0xBAFFCA, 0xBAFFDF,
+    0xBAFFEF, 0xBAFFFF, 0xBAEFFF, 0xBADFFF, 0xBACAFF, 0x000071, 0x1C0071, 0x390071, 0x550071,
+    0x710071, 0x710055, 0x710039, 0x71001C, 0x710000, 0x711C00, 0x713900, 0x715500, 0x717100,
+    0x557100, 0x397100, 0x1C7100, 0x007100, 0x00711C, 0x007139, 0x007155, 0x007171, 0x005571,
+    0x003971, 0x001C71, 0x393971, 0x453971, 0x553971, 0x613971, 0x713971, 0x713961, 0x713955,
+    0x713945, 0x713939, 0x714539, 0x715539, 0x716139, 0x717139, 0x617139, 0x557139, 0x457139,
+    0x397139, 0x397145, 0x397155, 0x397161, 0x397171, 0x396171, 0x395571, 0x394571, 0x515171,
+    0x595171, 0x615171, 0x695171, 0x715171, 0x715169, 0x715161, 0x715159, 0x715151, 0x715951,
+    0x716151, 0x716951, 0x717151, 0x697151, 0x617151, 0x597151, 0x517151, 0x517159, 0x517161,
+    0x517169, 0x517171, 0x516971, 0x516171, 0x515971, 0x000041, 0x100041, 0x200041, 0x310041,
+    0x410041, 0x410031, 0x410020, 0x410010, 0x410000, 0x411000, 0x412000, 0x413100, 0x414100,
+    0x314100, 0x204100, 0x104100, 0x004100, 0x004110, 0x004120, 0x004131, 0x004141, 0x003141,
+    0x002041, 0x001041, 0x202041, 0x282041, 0x312041, 0x392041, 0x412041, 0x412039, 0x412031,
+    0x412028, 0x412020, 0x412820, 0x413120, 0x413920, 0x414120, 0x394120, 0x314120, 0x284120,
+    0x204120, 0x204128, 0x204131, 0x204139, 0x204141, 0x203941, 0x203141, 0x202841, 0x2D2D41,
+    0x312D41, 0x352D41, 0x3D2D41, 0x412D41, 0x412D3D, 0x412D35, 0x412D31, 0x412D2D, 0x41312D,
+    0x41352D, 0x413D2D, 0x41412D, 0x3D412D, 0x35412D, 0x31412D, 0x2D412D, 0x2D4131, 0x2D4135,
+    0x2D413D, 0x2D4141, 0x2D3D41, 0x2D3541, 0x2D3141, 0x000000, 0x000000, 0x000000, 0x000000,
+    0x000000, 0x000000, 0x000000, 0x00000,
+];
